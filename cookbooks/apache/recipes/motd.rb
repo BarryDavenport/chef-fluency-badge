406538e64f2a318676414bfaba660f373e15f270
Barry <barry.davenport@perrie.com.au>
2017-05-07 10:48:03 +0000

    added motd and changed apache default for node attributes

---
+++ cookbooks/apache/recipes/motd.rb
@@ -0,0 +1,10 @@
+#####################################
+# motd.rb
+# ##################################
+#
+hostname = node['hostname']
+
+file '/etc/motd' do
+	content "Hostname is this: #{hostname}"
+end
+
